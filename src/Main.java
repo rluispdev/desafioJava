923f0173ff99aed2a5a2781b24e0d7f6d288bfe5
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-11 07:59:30 -0300

    Commit inicial e "banco de Dados"

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+//Banco de dados.
+        String name = "Rafael Gonzaga";
+        String accountType = "Corrente";
+        double avaliableBalance = 2500.00;
+
+
+
+    }
+}
