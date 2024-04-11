c1f6ff74411fa431e4c78242bacdb5e4d1ea85f5
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-11 11:08:17 -0300

    Criar o Loop

--- src/Main.java
+++ src/Main.java
@@ -11,49 +11,87 @@ public class Main {
 
         Scanner inputUser = new Scanner(System.in);
 
-        //Menu de Operações
-        String oparerations =  """
+        //Vizualizar saldo.
+        while (true) {
+            //Menu de Operações
+            String oparerations =  """
                 Operações
                 
                 1 - Consultar saldos
                 2 - Receber valor
                 3 - Transferir valor
-                4 - Sair
+                4 - Extrato
+                5 - Sair
                 
                 Digite a opção desejada:
                 """;
-        System.out.println(oparerations);
-        int menu = inputUser.nextInt();
+            System.out.println(oparerations);
+            int menu = inputUser.nextInt();
 
-        //Vizualizar saldo.
-        switch (menu){
-            case 1:
-                System.out.println("Seu saldo atual é de: " + avaliableBalance);
-                break;
-                
+            switch (menu) {
+                case 1:
+                    System.out.println("Seu saldo atual é de: " + avaliableBalance);
+                    break;
+
                 //Receber valor
-            case 2:
-                System.out.println("Qual é o valor que você vai receber?");
-                int receive = inputUser.nextInt();
-                  avaliableBalance += receive;
+                case 2:
+                    System.out.println("Qual é o valor que você vai receber?");
+                    int receive = inputUser.nextInt();
+                    avaliableBalance += receive;
 
-                System.out.println("R$ " + receive + " foram adicionados na sua conta corrente.");
-                System.out.println("Saldo Atualizado: R$ " + avaliableBalance);
-                  break;
+                    System.out.println("R$ " + receive + " foram adicionados na sua conta corrente.");
+//                    System.out.println("Saldo Atualizado: R$ " + avaliableBalance);
+                    break;
 
-                  //Transferir Valor
-            case 3 :
-                System.out.println("Qual o valor que você deseja transferir?");
-                int transfer = inputUser.nextInt();
-                avaliableBalance -= transfer;
+                //Transferir Valor
+                case 3:
+                    System.out.println("Qual o valor que você deseja transferir?");
+                    int transfer = inputUser.nextInt();
+                    avaliableBalance -= transfer;
 
-                System.out.println("R$ " + transfer + " foram transferidos.");
-                System.out.println("Saldo Atualizado: R$ " + avaliableBalance);
+                    System.out.println("R$ " + transfer + " foram transferidos.");
+//                    System.out.println("Saldo Atualizado: R$ " + avaliableBalance);
+                    break;
+
+                case 4:
+                    System.out.println("Essa feature será implementada em breve!");
+                    break;
+
+                case 5:
+                    System.out.println("Obrigado por usar nosso banco! Até mais!");
+                    break;
+
+                default:
+                    System.out.println("""
+                                Humm,  parace que essa opção  não é  um opção válida.
+              ______________________________                  
+              Caso queira;
+              Consultar  o saldo - Digite 1
+              Receber valor - Digite 2
+              Transferir valor - Digite 3
+              Extrato - Digite 4
+              Sair - Digite 5
+                               
+                               Acesse o Menu Inicial abaixo:
+                            """
+
+                    );
+
+            }
+            System.out.println("""
+            ___________________________________________
+            (1 - Sair       2 - Mostrar Menu Inicial)
+            """);
+            String exit = inputUser.next();
+
+            if (exit.equalsIgnoreCase("1")) {
+                System.out.println("Obrigado por usar nosso banco!");
                 break;
-            default:
-                System.out.println("Essa opção não existe no momento.");
-                break;
+            } else if (exit.equalsIgnoreCase("2")) {
+                continue;
+            } else {
+                System.out.println("Opção inválida. Digite 1 ou 2.");
+            }
         }
-
     }
 }
