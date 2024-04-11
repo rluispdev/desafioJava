9ce33ef56cc23248b3eec38d308d18dd16707fa8
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-11 08:09:50 -0300

    Criar o menu de operações.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,20 @@ public class Main {
         String accountType = "Corrente";
         double avaliableBalance = 2500.00;
 
+        //Menu de Operações
+        String menu = """
+                Operações
+                
+                1 - Consultar saldos
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair
+                
+                Digite a opção desejada:
+                """;
+
+
+
 
 
     }
